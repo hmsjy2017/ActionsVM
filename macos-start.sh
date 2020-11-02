2d93211bc5a4e84038bd4abbee368566642b083b
Tony <1462668901@qq.com>
2020-11-03 00:49:12 +0800

    Update macos-start.sh

--- macos-start.sh
+++ macos-start.sh
@@ -10,10 +10,3 @@ sudo systemsetup -settimezone "Asia/Shanghai"
 
 # Setting up VNC connection
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -off -restart -agent -privs -all -allowAccessFor -allUsers
-brew install screen
-screen -dmS ngrok_vnc
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip -O ngrok.zip
-unzip ngrok.zip ngrok
-rm ngrok.zip
-chmod +x ngrok
-sudo mv ngrok /usr/local/bin
